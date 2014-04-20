1cab56133029439a26d22bb66b549554bbfd6a2d
Gabriel <ggg.inbox@gmail.com>
2014-04-20 11:08:55 -0600

    Second Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dispinteresting::Application.config.secret_key_base = 'ab5582afa2121eaa7b67511f39bce987d5b7917deeaf717d8c70ae79bb7e8a51156ba06aecd816d3122b28086312d14220d56d9d7a796d52db74ee77b5ba773a'
